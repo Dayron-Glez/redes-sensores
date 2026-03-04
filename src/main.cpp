5785d1864ca3c7e10844ef0bd710989b7afe2ada
Dayron_Glez <dayronglezdev@gmail.com>
2026-03-04 18:12:00 +0100

    P1.3

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,29 @@ void IRAM_ATTR isrBoton()
 	botonPulsado = true;
 }
 
+void actualizarDisplay()
+{
+	Heltec.display->clear();
+	Heltec.display->setFont(ArialMT_Plain_16);
+	if (ledEncendido)
+	{
+		Heltec.display->drawString(0, 25, "ENCENDIDO");
+	}
+	else
+	{
+		Heltec.display->drawString(0, 25, "APAGADO");
+	}
+	Heltec.display->display();
+}
+
 void setup()
 {
-	Heltec.begin(false, false, true, false);
+	Heltec.begin(true, false, true, false);
 	pinMode(BOTON, INPUT_PULLUP);
 	pinMode(MI_LED, OUTPUT);
 	digitalWrite(MI_LED, LOW);
 	attachInterrupt(digitalPinToInterrupt(BOTON), isrBoton, FALLING);
+	actualizarDisplay();
 }
 
 void loop()
@@ -31,5 +47,6 @@ void loop()
 		digitalWrite(MI_LED, ledEncendido ? HIGH : LOW);
 		Serial.print("LED: ");
 		Serial.println(ledEncendido ? "ENCENDIDO" : "APAGADO");
+		actualizarDisplay();
 	}
 }
